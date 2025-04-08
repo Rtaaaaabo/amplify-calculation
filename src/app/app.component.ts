6602e147203fc77f0c648d43d867db9614b1d5bd
Taku Nakagawa <r.taaaaabo@gmail.com>
2025-04-08 23:53:39 +0900

    feat(ionic): variables.scssを適用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Amplify } from "aws-amplify";
+import { AdditionalComponent } from "./components/additional/additional.component";
 import outputs from "../../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
-  imports: [RouterOutlet],
+ selector: "app-root",
+ standalone: true,
+ templateUrl: "./app.component.html",
+ styleUrl: "./app.component.css",
+ imports: [RouterOutlet, AdditionalComponent],
 })
 export class AppComponent {
-  title = "amplify-angular-template";
+ title = "amplify-angular-template";
 }
