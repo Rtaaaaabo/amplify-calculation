9dc9fd93240e2821c7183b83b951c08d71f25d7e
Taku Nakagawa <r.taaaaabo@gmail.com>
2025-04-09 23:16:37 +0900

    feat(ionic): ionicのStyleを導入

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from "@angular/router";
 import { IonicModule } from "@ionic/angular";
 
 import { routes } from "./app.routes";
+import { provideIonicAngular } from "@ionic/angular/standalone";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(IonicModule.forRoot())],
+ providers: [
+  provideRouter(routes),
+  importProvidersFrom(IonicModule.forRoot()),
+  provideIonicAngular({}),
+ ],
 };
